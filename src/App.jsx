fcb391625d2363aa51d7b4500759cd887d3e172b
Dannizzzzz <oliviazhang096@gmail.com>
2023-06-06 10:30:12 +0930

    03-conditional-rendering/02-multiple-returns-fetch-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Example from './tutorial/03-conditional-rendering/01-multiple-returns-basics';
+import Example from './tutorial/03-conditional-rendering/02-multiple-returns-fetch-data';
 
 function App() {
   return (
